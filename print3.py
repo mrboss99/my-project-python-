43baddb4df1d22a8b68b3ef6748327fe0319d4ca
mrboss <44489689+mrboss99@users.noreply.github.com>
2018-10-25 23:54:51 -0700

    Update print3.py
    print characters and numbers in rows an columns

--- print3.py
+++ print3.py
@@ -1,7 +1,16 @@
-x=int(input("Tedad SOTON ra  vared konid:"))
-y=int(input("Tedad SATR ra vared konid: "))
+x=int(input("Enter number of columns:"))
+y=int(input("Enter number of  rows: "))
 y=y-1
 for i in range(x):
     print ("*")
     for y in range(y):
         print ("*",end="")
+#for print numbers :
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+x=int(input("Enter number of columns:"))
+y=int(input("Enter number of  rows: "))
+y=y-1
+for i in range(x):
+    print (i)
+    for y in range(y):
+        print (y,end="")
