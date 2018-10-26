0af9ec788da571003e8cf15a688779a716a7f69a
mrboss <44489689+mrboss99@users.noreply.github.com>
2018-10-25 23:50:07 -0700

    print 3

---
+++ print3.py
@@ -0,0 +1,7 @@
+x=int(input("Tedad SOTON ra  vared konid:"))
+y=int(input("Tedad SATR ra vared konid: "))
+y=y-1
+for i in range(x):
+    print ("*")
+    for y in range(y):
+        print ("*",end="")
